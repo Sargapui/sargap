3c3710640d8e7eb9dbcc1fd4053be791185f413a
unknown <sargapu98@gmail.com>
2023-03-22 16:54:52 +0400

    change something

--- src/App.js
+++ src/App.js
@@ -7,15 +7,18 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          اپلیکیشن طنین بزودی قابل دسترسی میباشد
         </p>
+        <p>
+           Tanin application will be available soon
+         </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          @SARGAP 2020-2023
         </a>
       </header>
     </div>
